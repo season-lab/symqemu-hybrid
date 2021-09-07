2827fb7273a122e8968e9cefe0c70e297d2090c9
Emilio Coppa <ercoppa@gmail.com>
2021-09-07 14:50:59 +0200

    disable debug

--- accel/tcg/hybrid/hybrid_debug.h
+++ accel/tcg/hybrid/hybrid_debug.h
@@ -1,14 +1,14 @@
 #ifndef HYBRID_DEBUG_H
 #define HYBRID_DEBUG_H
 
-#define HYBRID_SKIP_LIB_QUERY           1
+#define HYBRID_SKIP_LIB_QUERY           0
 #define HYBRID_DISABLE_Z3_SIMPLIFY      1
 #define HYBRID_DISABLE_RANGE_CONSTRAINT 1
 #define HYBRID_DBG_PRINT                1
 #define HYBRID_DBG_DUMP_QUERY           0
-#define HYBRID_DBG_CONSISTENCY_CHECK    1
-#define HYBRID_DBG_CONSISTENCY_ALT      1
-#define HYBRID_DBG_CHECK_PI_SAT         1
+#define HYBRID_DBG_CONSISTENCY_CHECK    0
+#define HYBRID_DBG_CONSISTENCY_ALT      0
+#define HYBRID_DBG_CHECK_PI_SAT         0
 #define HYBRID_DBG_PRINT_PC             0
 #define HYBRID_DBG_SKIP_SOLVE           0
 #define HYBRID_DBG_SKIP_QUERY           0
